8e6c0bd66a46df365c28ba2d7b9d74e4e2bfa650
Jorge-Pomo <jorge.pomo.sgpl@gmail.com>
2020-11-18 08:42:49 +0100

    primer commit

---
+++ P362_Navida.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class P362_Navida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		//VARIABLES
+		int numConsult = 0;
+		int dia = 0;
+		int mes = 0;
+		
+		
+		//ALGORITME
+		numConsult = sc.nextInt();
+		
+		for (int i = 0; i <= numConsult; i++) {
+			if() {
+				
+			}else {
+				
+			}
+		}
+		
+	}
+
+}
